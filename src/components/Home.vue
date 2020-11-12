a99214c3bc4de34dd8c36dd409ae0d303a93e555
Thapanee <it56160319@gmail.com>
2020-11-12 22:05:35 +0700

    Test
    Signed-off-by: Thapanee <it56160319@gmail.com>

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,7 +7,7 @@ export default {
     name: 'Home',
     data() {
         return {
-            home: 'Consequat occaecat ullamco amet non eiusmod nostrud dolore irure incididunt est duis anim sunt officia. Fugiat velit proident aliquip nisi incididunt nostrud exercitation proident est nisi. Irure magna elit commodo anim ex veniam culpa eiusmod id nostrud sit cupidatat in veniam ad. Eiusmod consequat eu adipisicing minim anim aliquip cupidatat culpa excepteur quis. Occaecat sit eu exercitation irure Lorem incididunt nostrud.'
+            home: '....Consequat occaecat ullamco amet non eiusmod nostrud dolore irure incididunt est duis anim sunt officia. Fugiat velit proident aliquip nisi incididunt nostrud exercitation proident est nisi. Irure magna elit commodo anim ex veniam culpa eiusmod id nostrud sit cupidatat in veniam ad. Eiusmod consequat eu adipisicing minim anim aliquip cupidatat culpa excepteur quis. Occaecat sit eu exercitation irure Lorem incididunt nostrud.'
         }
     },
 }
